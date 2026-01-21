6af9934767d6f185e530b123167bcacf7ed72c5c
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-01-21 18:58:20 +0000

    Feature: Add phone number validation function

--- config/Validation.php
+++ config/Validation.php
@@ -12,3 +12,12 @@
 function validateEmail($email) {
     return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
 }
+
+/**
+ * Validate phone number (basic validation for digits and hyphens)
+ * @param string $phone Phone number to validate
+ * @return bool True if valid phone format
+ */
+function validatePhone($phone) {
+    return preg_match('/^[0-9\-\+\(\)\s]{7,}$/', $phone) === 1;
+}
