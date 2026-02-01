4c7037a58cd99993de27452ac9f9e81b933b4d44
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-02-01 22:35:58 +0100

    Link CSS files to index.php and fix include paths

--- public/index.php
+++ public/index.php
@@ -1,15 +1,18 @@
-<?php
-    include("Header.html");
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/tables.css">
+    <link rel="stylesheet" href="css/forms.css">
 </head>
 <body>
+<?php
+    include("../src/views/layouts/Header.html");
+?>
     <h1>Welcome to my project</h1>
     <h1>in the Header you can access to each part of the site ⭡⭡ <br><br>
     or you can use the below links ⭣⭣ <br></h1>
@@ -19,16 +22,10 @@
         <a href="Path_Delete.php">Delete data from tables</a><br><br>
         <a href="Path_View.php">View the database tables</a><br><br>
     </h2>
-        
 
-
-    
 
 </body>
-</html>
-
-
-
 <?php
-    include("Footer.html");
+    include("../src/views/layouts/Footer.html");
 ?>
+</html>
