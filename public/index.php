fd73b08b487897b2e3d02d3dcdbf27fcaa22e6f2
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-02-05 11:50:20 +0100

    Link animations.css to index.php

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/tables.css">
     <link rel="stylesheet" href="css/forms.css">
+    <link rel="stylesheet" href="css/utilities.css">
+    <link rel="stylesheet" href="css/animations.css">
 </head>
 <body>
 <?php
