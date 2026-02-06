4c3a14c6b749b378ad599d2e3c5033fc81e27ed7
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-02-06 16:29:10 +0100

    Improve Landlord page structure with better heading and description

--- public/Mapp_Landlord.php
+++ public/Mapp_Landlord.php
@@ -15,9 +15,21 @@
 <?php
     include("Header.html");
 ?>
-<h1>Landlord Page</h1>
+<main class="container">
+    <section class="page-header">
+        <h1>Landlord Management</h1>
+        <p class="subtitle">Manage all landlord information and details</p>
+    </section>
 
-    <h2>Please follow the links!</h2><br><br>
+    <section class="landlord-info">
+        <h2>Landlord Information</h2>
+        <p>The Landlord Management system allows you to:</p>
+        <ul>
+            <li>Add new landlords with contact information</li>
+            <li>View all registered landlords in the system</li>
+            <li>Delete landlord records when needed</li>
+        </ul>
+    </section>
     <h2>
         For Add a new Landlord <a href="Add_Landlord.php">click here</a><br><br>
         For Delete a Landlord <a href="Del_Landlord.php">click here</a><br><br>
