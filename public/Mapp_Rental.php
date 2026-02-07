e64bfea01c583374317fb7b505844afa3a9cc7c8
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-02-07 22:39:44 +0100

    Improve Rental page structure with better heading and description

--- public/Mapp_Rental.php
+++ public/Mapp_Rental.php
@@ -15,9 +15,21 @@
 <?php
     include("Header.html");
 ?>
-<h1>Rental Page</h1>
+<main class="container">
+    <section class="page-header">
+        <h1>Rental Management</h1>
+        <p class="subtitle">Manage all rental agreements and details</p>
+    </section>
 
-    <h2>Please follow the links!</h2><br><br>
+    <section class="rental-info">
+        <h2>Rental Information</h2>
+        <p>The Rental Management system allows you to:</p>
+        <ul>
+            <li>Create new rental agreements with start/end dates and terms</li>
+            <li>View all active and past rentals in the system</li>
+            <li>Delete rental records when agreements end</li>
+        </ul>
+    </section>
     <h2>
         For Add a new Rental <a href="Add_Rental.php">click here</a><br><br>
         For Delete a Rental <a href="Del_Rental.php">click here</a><br><br>
