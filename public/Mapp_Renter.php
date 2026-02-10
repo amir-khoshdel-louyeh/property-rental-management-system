f5f53b020caef8f588d0ce8c470318ae35ad5cbe
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-02-10 06:10:59 +0100

    Improve Renter page structure with better heading and description

--- public/Mapp_Renter.php
+++ public/Mapp_Renter.php
@@ -15,9 +15,21 @@
 <?php
     include("Header.html");
 ?>
-<h1>Renter Page</h1>
+<main class="container">
+    <section class="page-header">
+        <h1>Renter Management</h1>
+        <p class="subtitle">Manage all renter information and details</p>
+    </section>
 
-    <h2>Please follow the links!</h2><br><br>
+    <section class="renter-info">
+        <h2>Renter Information</h2>
+        <p>The Renter Management system allows you to:</p>
+        <ul>
+            <li>Add new renters with personal and contact information</li>
+            <li>View all registered renters in the system</li>
+            <li>Delete renter records when needed</li>
+        </ul>
+    </section>
     <h2>
         For Add a new Renter <a href="Add_Renter.php">click here</a><br><br>
         For Delete a Renter <a href="Del_Renter.php">click here</a><br><br>
