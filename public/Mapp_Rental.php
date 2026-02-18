985c8fb89d1b64e94de7de16f9156cfa80fc53a2
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-02-18 16:19:02 +0100

    Updated Rental.php and remove redundant files

--- public/Mapp_Rental.php
+++ public/Mapp_Rental.php
@@ -36,19 +36,19 @@
             <div class="action-card add-action">
                 <h3>Create New Rental</h3>
                 <p>Set up a new rental agreement with all rental details</p>
-                <a href="Add_Rental.php" class="btn btn-primary">Add Rental</a>
+                <a href="Rental.php" class="btn btn-primary">Manage Rentals</a>
             </div>
 
             <div class="action-card view-action">
                 <h3>View All Rentals</h3>
                 <p>Browse the complete list of all rental agreements</p>
-                <a href="Show_Rental.php" class="btn btn-info">View Rentals</a>
+                <a href="Rental.php" class="btn btn-info">Manage Rentals</a>
             </div>
 
             <div class="action-card delete-action">
                 <h3>Delete Rental</h3>
                 <p>Remove a rental agreement from the system</p>
-                <a href="Del_Rental.php" class="btn btn-danger">Delete Rental</a>
+                <a href="Rental.php" class="btn btn-danger">Manage Rentals</a>
             </div>
         </div>
     </section>
