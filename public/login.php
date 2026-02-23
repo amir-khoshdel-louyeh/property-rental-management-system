98ba3c197b489f3fc3a2b4ba73eaf5f564cbf2e0
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-02-23 08:21:00 +0100

    Create login form page

---
+++ public/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Property & Rental Management System</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/forms.css">
+    <link rel="stylesheet" href="css/utilities.css">
+</head>
+<body>
+    <?php include 'Header.html'; ?>
+    
+    <div class="container">
+        <div class="form-container">
+            <h2>Login</h2>
+            
+            <?php
+            if (isset($_GET['error'])) {
+                echo '<div class="alert alert-error">' . htmlspecialchars($_GET['error']) . '</div>';
+            }
+            if (isset($_GET['success'])) {
+                echo '<div class="alert alert-success">' . htmlspecialchars($_GET['success']) . '</div>';
+            }
+            ?>
+            
+            <form action="login_process.php" method="POST">
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+            </form>
+            
+            <p>Don't have an account? <a href="register.php">Register here</a></p>
+        </div>
+    </div>
+    
+    <?php include 'Footer.html'; ?>
+</body>
+</html>
