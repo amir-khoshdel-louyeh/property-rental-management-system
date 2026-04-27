4ef8afd6e513ff168fc284e64c42fb75860f0759
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-04-27 19:30:53 +0200

    refactor: reuse shared head include in payment rental renter map pages

--- public/Mapp_Rental.php
+++ public/Mapp_Rental.php
@@ -4,12 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rental</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/tables.css">
-    <link rel="stylesheet" href="css/forms.css">
-    <link rel="stylesheet" href="css/utilities.css">
-    <link rel="stylesheet" href="css/animations.css">
+<?php include("layouts/CommonHeadLinks.php"); ?>
 </head>
 <body>
 <?php
