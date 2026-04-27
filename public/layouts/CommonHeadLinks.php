8347798f9949ed7a1bae841256ae132cdad6e2ee
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-04-27 19:30:53 +0200

    refactor: extract shared Mapp page head styles include

---
+++ public/layouts/CommonHeadLinks.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/header.css">
+<link rel="stylesheet" href="css/tables.css">
+<link rel="stylesheet" href="css/forms.css">
+<link rel="stylesheet" href="css/utilities.css">
+<link rel="stylesheet" href="css/animations.css">
